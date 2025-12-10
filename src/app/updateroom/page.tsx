74ab0c6de2258be0dd72a800c68789b1ddd226db
chaunguye <trinhchau1204@gmail.com>
2025-12-10 15:04:31 +0700

    Update Booking fix URL

--- src/app/updateroom/page.tsx
+++ src/app/updateroom/page.tsx
@@ -4,22 +4,25 @@ import ProtectedRoute from "@/components/ProtectedRoute";
 import UpdateRoom from "@/features/room_service/UpdateRoom";
 import { BookingInfo } from "@/type/type";
 
-const mockdata: BookingInfo = {
-  id: "1",
-  room: "101",
-  date: "2024-06-15",
-  startTime: "10:00",
-  endTime: "12:00",
-  courseName: "Computer Network",
-  notes: "Project discussion"
-};
+// const mockdata: BookingInfo = {
+//   id: "1",
+//   room: "101",
+//   date: "2024-06-15",
+//   startTime: "10:00",
+//   endTime: "12:00",
+//   courseName: "Computer Network",
+//   notes: "Project discussion"
+// };
 
 export default function BookRoomPage() {
   return (
+    // <ProtectedRoute>
+    //   <UpdateRoom existingBooking={mockdata} onBookingUpdate={(updatedBooking) => {
+    //     console.log("Updated Booking:", updatedBooking);
+    //   }} />
+    // </ProtectedRoute>
     <ProtectedRoute>
-      <UpdateRoom existingBooking={mockdata} onBookingUpdate={(updatedBooking) => {
-        console.log("Updated Booking:", updatedBooking);
-      }} />
+      <UpdateRoom />
     </ProtectedRoute>
   );
 }
